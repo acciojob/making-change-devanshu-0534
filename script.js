60629c427c6acda1948eed3c92de225c68afef74
Devanshu Shyamsundar <132264393+devanshu-0534@users.noreply.github.com>
2026-01-07 14:53:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 const makeChange = (c) => {
   // your name here
+  let change = {
+    q: 0,
+    d: 0,
+    n: 0,
+    p: 0
+  };
+
+  // quarters
+  change.q = Math.floor(c / 25);
+  c = c % 25;
+
+  // dimes
+  change.d = Math.floor(c / 10);
+  c = c % 10;
+
+  // nickels
+  change.n = Math.floor(c / 5);
+  c = c % 5;
+
+  // pennies
+  change.p = c;
+
+  return change;
 };
 
 // Do not the change the code below
